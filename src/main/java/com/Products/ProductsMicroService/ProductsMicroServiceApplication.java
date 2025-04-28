042f13691f711845088d0a54fdaf711e43f78f86
ryszardpanda <rychu48@op.pl>
2025-04-28 18:12:26 +0200

    initial commit

---
+++ src/main/java/com/Products/ProductsMicroService/ProductsMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package com.Products.ProductsMicroService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsMicroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductsMicroServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Products/ProductsMicroService/ProductsMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Products.ProductsMicroService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsMicroServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

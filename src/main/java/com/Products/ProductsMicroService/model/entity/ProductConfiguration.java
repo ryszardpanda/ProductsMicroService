acb6b505ce07524cc447c035c9e96a40edc599f7
ryszardpanda <rychu48@op.pl>
2025-05-04 23:30:29 +0200

    ++changed auto generated equals and hash code for Entities

--- src/main/java/com/Products/ProductsMicroService/model/entity/ProductConfiguration.java
+++ src/main/java/com/Products/ProductsMicroService/model/entity/ProductConfiguration.java
@@ -2,15 +2,18 @@ package com.Products.ProductsMicroService.model.entity;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
 
-@Data
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@Getter
+@Setter
 public class ProductConfiguration {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -30,4 +33,22 @@ public class ProductConfiguration {
     @CollectionTable(name = "product_accessories", joinColumns = @JoinColumn(name = "product_id"))
     @Column(name = "accessory")
     private List<String> accessories;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+
+        if (!(o instanceof ProductConfiguration))
+            return false;
+
+        ProductConfiguration other = (ProductConfiguration) o;
+
+        return id != null &&
+                id.equals(other.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
--- src/main/java/com/Products/ProductsMicroService/model/entity/ProductEntity.java
+++ src/main/java/com/Products/ProductsMicroService/model/entity/ProductEntity.java
@@ -3,13 +3,15 @@ package com.Products.ProductsMicroService.model.entity;
 import com.Products.ProductsMicroService.common.ProductsType;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@Getter
+@Setter
 public class ProductEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -23,4 +25,22 @@ public class ProductEntity {
 
     @OneToOne(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
     private ProductConfiguration configuration;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+
+        if (!(o instanceof ProductEntity))
+            return false;
+
+        ProductEntity other = (ProductEntity) o;
+
+        return id != null &&
+                id.equals(other.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }

51750a2f53280c7c5ccf7c70de0af58c5246253b
ryszardpanda <rychu48@op.pl>
2025-05-23 17:59:18 +0200

    ++Added dockerfile

--- src/main/java/com/Products/ProductsMicroService/model/entity/ProductConfiguration.java
+++ src/main/java/com/Products/ProductsMicroService/model/entity/ProductConfiguration.java
@@ -20,7 +20,7 @@ public class ProductConfiguration {
     private Long id;
 
     @OneToOne
-    @JoinColumn(name = "product_id", nullable = false)
+    @JoinColumn(name = "product_id")
     private ProductEntity product;
 
     private String processor;

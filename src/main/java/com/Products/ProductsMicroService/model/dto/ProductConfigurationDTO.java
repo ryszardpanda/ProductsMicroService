dd3d671084e99e71ed68b3dfce9abf5789c22077
ryszardpanda <rychu48@op.pl>
2025-06-04 17:05:43 +0200

    ++changed model for product and product configuration

--- src/main/java/com/Products/ProductsMicroService/model/dto/ProductConfigurationDTO.java
+++ src/main/java/com/Products/ProductsMicroService/model/dto/ProductConfigurationDTO.java
@@ -11,7 +11,7 @@ import java.math.BigDecimal;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ProductConfigurationDTO {
-    private Long id;        // null → utwórz nową
+    private Long id;
     private String name;
     private BigDecimal price;
     private ConfigurationType type;
